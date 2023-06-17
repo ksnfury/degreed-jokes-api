5d1c66ff18580dc82c08cf23c78356c813c3345f
Karan Nanda <ksnfury@gmail.com>
2023-06-17 12:29:45 +0530

    Preparing Joke.cs for ORM

--- Models/Joke.cs
+++ Models/Joke.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JokeApi.Models
 {
     public class Joke
     {
+        [Key]
         public int Id { get; set; }
-        public string? Text { get; set; }
+
+        [Required]
+        public string Text { get; set; }
         public int Length { get; set; }
     }
 }
