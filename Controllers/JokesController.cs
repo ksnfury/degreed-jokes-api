669453d54f50b65cb2f2b05ba9f692592f6947bf
Karan Nanda <ksnfury@gmail.com>
2023-06-17 12:38:16 +0530

    Adding validations to JokesController and using JokeDto for transfer to UI

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -1,29 +1,61 @@
 using Microsoft.AspNetCore.Mvc;
 using JokeApi.Services;
+using JokeApi.Models;
 
 [ApiController]
 [Route("api/v1/[controller]")]
 [ApiVersion("1.0")]
 public class JokesController : ControllerBase
+{
+    private readonly IJokeService _jokeService;
+
+    public JokesController(IJokeService jokeService)
     {
-        private readonly IJokeService _jokeService;
-
-        public JokesController(IJokeService jokeService)
-        {
-            _jokeService = jokeService;
-        }
-
-        [HttpGet("random")]
-        public IActionResult GetRandomJoke()
-        {
-            var joke = _jokeService.GetRandomJoke();
-            return Ok(joke);
-        }
-
-        [HttpGet]
-        public IActionResult SearchJokes(string searchTerm)
-        {
-            var jokes = _jokeService.SearchJokes(searchTerm);
-            return Ok(jokes);
-        }
+        _jokeService = jokeService;
     }
+
+    [HttpGet("random")]
+    public ActionResult<JokeDto> GetRandomJoke()
+    {
+        var joke = _jokeService.GetRandomJoke();
+        if (joke == null)
+            return NotFound();
+
+        var jokeDto = MapToDto(joke);
+        return jokeDto;
+    }
+
+    [HttpGet]
+    public ActionResult<Dictionary<JokeLengthCategory, List<JokeDto>>> SearchJokes([FromQuery] string searchTerm)
+    {
+
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            return BadRequest("Search term cannot be empty");
+        }
+
+        var jokes = _jokeService.SearchJokes(searchTerm);
+
+        if (jokes.Count == 0)
+            return NotFound();
+
+        var jokeDtos = new List<JokeDto>();
+        foreach (var joke in jokes)
+        {
+            var jokeDto = MapToDto(joke);
+            jokeDtos.Add(jokeDto);
+        }
+
+        return jokeDtos;
+    }
+
+    private JokeDto MapToDto(Joke joke)
+    {
+        return new JokeDto
+        {
+            Id = joke.Id,
+            Text = joke.Text,
+            Length = joke.Length
+        };
+    }
+}
